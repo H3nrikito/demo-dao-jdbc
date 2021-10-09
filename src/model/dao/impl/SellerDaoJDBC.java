142a8affa91b5b0fb8dd8f64b0e8d9d0043ba10f
H3nrikito <jhowhp@yahoo.com.br>
2021-10-09 10:19:43 -0300

    delete implementation

--- src/model/dao/impl/SellerDaoJDBC.java
+++ src/model/dao/impl/SellerDaoJDBC.java
@@ -87,14 +87,34 @@ public class SellerDaoJDBC implements SellerDao {
 		}
 		finally {
 			DB.closeStatement(st);
-			
 		}
-
 	}
 
 	@Override
 	public void deleteById(Integer id) {
-		// TODO Auto-generated method stub
+		
+		PreparedStatement st = null;
+
+		try {
+			st = conn.prepareStatement("DELETE FROM seller WHERE Id = ?");
+			
+			st.setInt(1, id);
+			int rows = st.executeUpdate();
+			
+			if(rows == 0) { //if opcional para verificar se alguma linha foi deletada na base
+				throw new DbException("No one rows ");
+			}
+			
+			
+		}
+		catch(SQLException e){
+			throw new DbException(e.getMessage());
+		}
+		finally {
+			DB.closeStatement(st);
+		}
+		
+	
 
 	}
 
